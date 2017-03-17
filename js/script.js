352d9771677cf046b90eb85eab655e64fcf0fded
Anna Lamoureux <anna.lamoureux@gmail.com>
2017-03-16 20:45:39 -0400

    Adds hover tooltips for course-related websites to warn about quality of site

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
 $(document).ready(function(){
     
+    $(".tooltip").tooltipster({
+        theme: "tooltipster-shadow"
+    });
+    
 	var $w = $(window).scroll(function(){
-        if($w.scrollTop() >= 100) {   
-            $("nav").addClass("scroll-mode");
-        }
-        else {
-            $("nav").removeClass("scroll-mode");
+        if ($w.width > 500) {
+           if($w.scrollTop() >= 100) {   
+                $("nav").addClass("scroll-mode");
+            }
+            else {
+                $("nav").removeClass("scroll-mode");
+            } 
         }
         
+        if($w.scrollTop <= 700) {
+            $("#nav-home").addClass("active");
+            $("#nav-about").removeClass("active");
+            $("#nav-exp").removeClass("active");
+            $("#nav-cont").removeClass("active");
+        }
+
+        if($w.scrollTop > 700 && $w.scrollTop <= 1500) {
+            $("#nav-home").removeClass("active");
+            $("#nav-about").addClass("active");
+            $("#nav-exp").removeClass("active");
+            $("#nav-cont").removeClass("active");
+        }
+
+        if($w.scrollTop > 1500 && $w.scrollTop <= 3000) {
+            $("#nav-home").removeClass("active");
+            $("#nav-about").removeClass("active");
+            $("#nav-exp").addClass("active");
+            $("#nav-cont").removeClass("active");
+        }
+
+        if($w.scrollTop > 3000) {
+            $("#nav-home").removeClass("active");
+            $("#nav-about").removeClass("active");
+            $("#nav-exp").removeClass("active");
+            $("#nav-cont").addClass("active");
+        }
+
         if($w.scrollTop() >= 2200) { 
             $("#skillbar_java").animate({width:'85%'},1500);
             $("#skillbar_py").animate({width:'80%'},1500);
@@ -33,7 +67,5 @@ $(document).ready(function(){
                 }
             }
         });
-    });
-    
-    
+    }); 
 });
