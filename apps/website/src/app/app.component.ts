b2acc8a7d39b3f8afe61d4852d7df2248228153b
anna-lamoureux <anna.lamoureux@gmail.com>
2022-08-08 20:12:26 -0500

    Replaces old website with fresh angular application.

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'anna-lamoureux.github.io-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'website';
+}
